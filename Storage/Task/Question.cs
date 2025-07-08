3ae22c534fb1309ed7ab7cce0dc7d5deec586708
bereskristof <113383819+bereskristof@users.noreply.github.com>
2025-07-08 12:02:27 +0200

    [Save guarantee] Better question class

--- Storage/Task/Question.cs
+++ Storage/Task/Question.cs
@@ -5,88 +5,183 @@ namespace Storage.Task;
 
 public class Question : INotifyPropertyChanged
 {
+    const int DefaultPoints = 1;
+    
     public event PropertyChangedEventHandler? PropertyChanged;
     
-    private string _text = string.Empty;
+    private string _storedText; // The string stored in the database
+    private string? _bufferedText; // The string currently being edited in the UI
     
-    private AnswerList _answers = [];
+    private int _storedPoints; // The points' amount stored in the database
+    private int _bufferedPoints; // The points' amount currently being edited in the UI
+    
+    private bool _isConfirmedImageless; // Indicates if an image was confirmed to be absent for this question, used to prevent unnecessary refetches from the database
+    
+    private readonly AnswerList _answers = []; // The list of answers for this question, loaded lazily
     
     public long Id { get; }
 
+    /// The text of the question, fetched and decrypted from the database as needed.
     public string Text
     {
-        get => _text;
-        set
-        {
-            _text = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
-        }
+        get => GetText();
+        set => SetText(value);
+    }
+
+    /// The point amount for this question.
+    public int Points
+    {
+        get => GetPoints(); 
+        set => SetPoints(value);
     }
     
-    public int Points { get; set; }
-    
+    /// Indicates whether the question text failed to decrypt.
     public bool FailedToDecrypt { get; private set; }
 
-    public AnswerList Answers
+    /// The list of answers for this question, loaded lazily.
+    public AnswerList Answers => GetAnswers();
+
+    // private set => _answers = value;
+    /// Gets the question text, fetches and decrypts it from the database if necessary.
+    /// NOTE: Assumes the text is always correct after the first read.
+    /// Essentially, `text` is only read once.
+    private string GetText()
     {
-        get
+        if (_bufferedText != null) return _bufferedText; // If the text is already buffered, return it directly
+        var readCommand = Manager.Connection.CreateCommand();
+        readCommand.CommandText = "SELECT Question FROM Questions WHERE Id = @Id";
+        readCommand.Parameters.AddWithValue("@Id", Id);
+        using var reader = readCommand.ExecuteReader();
+        if (!reader.Read())
         {
-            if (_answers.Count != 0) return _answers;
-            _answers.LoadAll(this);
-            return _answers;
+            Log.Write($"GetText: Question {Id} not in database", Log.Severity.Error);
+            return string.Empty;
         }
-        private set => _answers = value;
+        string encryptedText = reader.GetString(0);
+        bool corrupted = !CanDecryptText(encryptedText, out string decryptedText);
+        if (corrupted) Log.Write($"GetText: Question {Id} value is corrupted", Log.Severity.Error);
+        FailedToDecrypt = corrupted;
+        _storedText = decryptedText;
+        _bufferedText = decryptedText;
+        // PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+        Log.Write($"GetText: Question {Id} text fetched from database");
+        return decryptedText;
     }
 
-    private Question(long id, string text, int points)
+    /// Updates buffered `text` and stores' it in the database.
+    private void SetText(string value)
+    {
+        _bufferedText = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+        if (_storedText == _bufferedText) { // No change, no need to update the database
+            Log.Write($"SetText: Question {Id} text update skipped");
+            return;
+        }
+        var updateCommand = Manager.Connection.CreateCommand();
+        updateCommand.CommandText = "UPDATE Questions SET Question = @Question WHERE Id = @Id";
+        string encryptedText = Encryption.EncryptBase64(value);
+        updateCommand.Parameters.AddWithValue("@Question", encryptedText);
+        updateCommand.Parameters.AddWithValue("@Id", Id);
+        updateCommand.ExecuteNonQuery();
+        _storedText = value;
+        Log.Write($"SetText: Question {Id} text stored in the database");
+    }
+
+    /// Gets the points' amount, fetches it from the database if necessary.
+    /// NOTE: Assumes points from construction are correct, period.
+    private int GetPoints()
+    {
+        return _bufferedPoints; // If the text is already buffered, return it directly
+    }
+
+    /// Updates buffered `text` and stores' it in the database.
+    private void SetPoints(int value)
+    {
+        _bufferedPoints = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Points)));
+        if (_storedPoints == _bufferedPoints) { // No change, no need to update the database
+            Log.Write($"SetPoints: Question {Id} points update skipped");
+            return;
+        }
+        var updateCommand = Manager.Connection.CreateCommand();
+        updateCommand.CommandText = "UPDATE Questions SET Points = @Points WHERE Id = @Id";
+        updateCommand.Parameters.AddWithValue("@Points", value);
+        updateCommand.Parameters.AddWithValue("@Id", Id);
+        updateCommand.ExecuteNonQuery();
+        _storedPoints = value;
+        Log.Write($"SetPoints: Question {Id} points stored in the database");
+    }
+    
+    private AnswerList GetAnswers()
+    {
+        if (_answers.Count != 0) return _answers;
+        _answers.LoadAll(this);
+        return _answers;
+    }
+
+    private Question(long id, string text, int points, bool failedToDecrypt)
     {
         Id = id;
-        Text = text;
-        Points = points;
+        _bufferedText = text;
+        _storedText = text;
+        _bufferedPoints = points;
+        _storedPoints = points;
+        FailedToDecrypt = failedToDecrypt;
     }
 
+    /// Used for batch loading, where `text` is already read, but not yet decrypted.
     internal static Question Load(long id, string text, int points)
     {
-        string decryptedText;
-        bool corrupted = false;
+        bool corrupted = !CanDecryptText(text, out string decryptedText);
+        var question = new Question(id, decryptedText, points, corrupted);
+        return question;
+    }
+    
+    /// Decrypts the provided text, returns true if the text was successfully decrypted.
+    private static bool CanDecryptText(string text, out string decryptedText)
+    {
+        bool isDecrypted = false;
         try
         {
             decryptedText = Encryption.DecryptBase64(text);
+            isDecrypted = true;
         }
         catch (Exception e) when (e is CryptographicException or FormatException)
         {
             decryptedText = "[DAMAGED] " + text;
-            corrupted = true;
         }
-        var question = new Question(id, decryptedText, points);
-        if (corrupted) question.FailedToDecrypt = true;
-        return question;
+        return isDecrypted;
     }
 
+    /// Creates a new empty question, used when adding a new question in the UI, immediately stores it in the database.
     internal static Question New()
     {
         var createCommand = Manager.Connection.CreateCommand();
-        createCommand.CommandText = "INSERT INTO Questions (Id, Points, Question) VALUES (@Id, 0, '');";
+        createCommand.CommandText = "INSERT INTO Questions (Id, Points, Question) VALUES (@Id, @Points, '');";
         var newId = IdManager.GetQuestionId();
         createCommand.Parameters.AddWithValue("@Id", newId);
+        createCommand.Parameters.AddWithValue("@Points", DefaultPoints);
         createCommand.ExecuteScalar();
-        return new Question(newId, "", 0);
+        return new Question(newId, string.Empty, DefaultPoints, false);
     }
 
+    [Obsolete("Planning to rework this class to save data on it's own, without the need for a separate Store method.")]
     public void Store()
     {
-        var updateCommand = Manager.Connection.CreateCommand();
-        updateCommand.CommandText = $"UPDATE Questions SET Question = @Question, Points = @Points WHERE Id = @Id";
-        string encryptedText = Encryption.EncryptBase64(Text);
-        updateCommand.Parameters.AddWithValue("@Question", encryptedText);
-        updateCommand.Parameters.AddWithValue("@Points", Points);
-        updateCommand.Parameters.AddWithValue("@Id", Id);
-        updateCommand.ExecuteNonQuery();
-        foreach (var answer in Answers) answer.Store();
-        Log.Write($"Question {Id} stored");
-        FailedToDecrypt = false;
+        return;
+        // var updateCommand = Manager.Connection.CreateCommand();
+        // updateCommand.CommandText = $"UPDATE Questions SET Question = @Question, Points = @Points WHERE Id = @Id";
+        // string encryptedText = Encryption.EncryptBase64(Text);
+        // updateCommand.Parameters.AddWithValue("@Question", encryptedText);
+        // updateCommand.Parameters.AddWithValue("@Points", Points);
+        // updateCommand.Parameters.AddWithValue("@Id", Id);
+        // updateCommand.ExecuteNonQuery();
+        // foreach (var answer in Answers) answer.Store();
+        // Log.Write($"Question {Id} stored");
+        // FailedToDecrypt = false;
     }
 
+    /// Removes the question from the database, deletes all answers and image associated with it.
     public void Delete()
     {
         DeleteImage();
@@ -96,16 +191,23 @@ public class Question : INotifyPropertyChanged
         deleteCommand.CommandText = $"DELETE FROM Questions WHERE Id = @Id";
         deleteCommand.Parameters.AddWithValue("@Id", Id);
         deleteCommand.ExecuteNonQuery();
-        Log.Write($"Question {Id} deleted");
+        Log.Write($"Delete: Question {Id} deleted");
     }
 
+    /// Fetches the image associated with this question from the database, decrypts it and returns a MemoryStream.
     public MemoryStream? FetchImageStream()
     {
+        if (_isConfirmedImageless) return null;
         var fetchCommand = Manager.Connection.CreateCommand();
         fetchCommand.CommandText = $"SELECT Image FROM Images WHERE QuestionId = @QuestionId";
         fetchCommand.Parameters.AddWithValue("@QuestionId", Id);
         using var reader = fetchCommand.ExecuteReader();
-        if (!reader.Read()) return null;
+        if (!reader.Read())
+        {
+            Log.Write($"FetchImageStream: No image found for question {Id}");
+            _isConfirmedImageless = true;
+            return null;
+        }
         using var dataStream = reader.GetStream(0);
         var memoryStream = new MemoryStream();
         dataStream.CopyTo(memoryStream);
@@ -114,6 +216,7 @@ public class Question : INotifyPropertyChanged
         return cleanMemoryStream;
     }
 
+    /// Stores an image from a file path into the database, encrypts it and associates it with this question.
     public void StoreImageFromPath(string path)
     {
         byte[] data;
@@ -123,7 +226,7 @@ public class Question : INotifyPropertyChanged
         }
         catch
         {
-            Log.Write($"Failed to add image from path: {path}");
+            Log.Write($"StoreImageFromPath: Failed to add image from path to question {Id}", Log.Severity.Error);
             return;
         }
         byte[] encryptedData = Encryption.EncryptBlob(data);
@@ -133,15 +236,16 @@ public class Question : INotifyPropertyChanged
         storeCommand.Parameters.AddWithValue("@Image", encryptedData);
         DeleteImage();
         storeCommand.ExecuteNonQuery();
-        Log.Write($"Image from path {path} added to question {Id}");
+        Log.Write($"StoreImageFromPath: Image added to question {Id}");
     }
     
+    /// Deletes the image associated with this question from the database.
     public void DeleteImage()
     {
         var deleteCommand = Manager.Connection.CreateCommand();
         deleteCommand.CommandText = "DELETE FROM Images WHERE QuestionId = @QuestionId";
         deleteCommand.Parameters.AddWithValue("@QuestionId", Id);
         deleteCommand.ExecuteNonQuery();
-        Log.Write($"Image for question {Id} deleted");
+        Log.Write($"DeleteImage: Image for question {Id} deleted");
     }
 }
